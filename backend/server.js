5ea692979229b3c039b7a6f0b4b992bd1b02d33d
SrushtyR <srushtyr@Srushtys-MacBook-Air.local>
2024-02-14 16:16:09 -0500

    Added login,signup pages

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./data/data");
+const connectDB = require("./config/db");
+const colors = require("colors");
+
+dotenv.config();
+connectDB();
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+  //   console.log(req);
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, console.log(`Server started on PORT ${PORT}`.yellow.bold));
